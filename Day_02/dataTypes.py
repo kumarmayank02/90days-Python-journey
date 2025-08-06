d9380f1aec2fb1a7a102b217d395ebcaaeb708e9
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-06 08:00:04 +0000

    variables and datatypes in python

---
+++ Day_02/dataTypes.py
@@ -0,0 +1,77 @@
+#About DataTypes
+#Examples of Datatypes
+#implements of datatypes
+
+'''What is a Data Type?
+Data type specifies the type of value a variable holds. This is required in programming to do various operations without causing an error.
+In python, we can print the type of any operator using type function:'''
+
+a = 1
+print(type(a))
+b = "1"
+print(type(b))
+
+d=3.5
+e="kumar"
+print("type of d",type(d));
+print("type of e",type(e));
+
+
+#complex number
+f=complex(5,2)
+print(f);
+
+##int
+e=2
+print(e)
+
+#String
+
+g="This is Kumar Mayank"
+print(g)
+
+#boolean
+h=True
+print(h)
+
+#Sequenced data:list,tuple
+list1  = [9,3,4.6],[-8,9]
+["apple","Orange"]
+print(list1)
+
+tuple1 =(("parrot",
+          "sparrow"),("lion","Elephant"))
+print(tuple1)
+
+
+#Mapped data:dict
+
+dict1={"name":"Mayank","age":20,"canEligible":True}
+print(dict1)
+
+
+
+#NOTES OF ALL DATATYPES 
+
+'''(1). Numeric data: int, float, complex
+int: 3, -8, 0
+float: 7.349, -9.0, 0.0000001
+complex: 6 + 2i
+(2). Text data: str
+str: "Hello World!!!", "Python Programming"
+
+(3). Boolean data:
+Boolean data consists of values True or False.
+
+(4). Sequenced data: list, tuple
+list: A list is an ordered collection of data with elements separated by a comma and enclosed within square brackets. Lists are mutable and can be modified after creation.
+
+uple: A tuple is an ordered collection of data with elements separated by a comma and enclosed within parentheses. Tuples are immutable and can not be modified after creation.
+
+(5). Mapped data: dict
+dict: A dictionary is an unordered collection of data containing a key:value pair. The key:value pairs are enclosed within curly brackets.'''
+
+
+
+
+
---
+++ Day_02/variables.py
@@ -0,0 +1,41 @@
+#About Variables
+#Examples Of Variables
+#Logics based Example on  Variables
+
+#**About Variables***
+'''Variable is like a container that holds data. Very similar to how our containers in kitchen 
+holds sugar, salt etc Creating a variable is like creating a placeholder
+ in memory and assigning it some value. In Python its as easy as writing:'''
+
+a = 1
+b = True
+c = "Harry"
+d = None
+'''These are four variables of different data types.'''
+
+
+print(a,b,c,d); '''We can print the one by one but we can print by one method'''
+
+'''More examples'''
+a=99932093039
+print(a);
+
+b="kumar Mayank"
+
+print(b);
+
+c=7893.8
+print(c);
+
+
+d=787878833993093039.9
+print(d);
+
+f="i love python"
+print(f)
+
+
+'''Abb humko alag variables ko store krna h to data type ke through kr skte h'''
+
+
+
