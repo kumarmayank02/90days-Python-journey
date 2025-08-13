eaff94a21f3350377c509b73aee8489a1ed81131
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-13 06:33:08 +0000

    CalculatorbyDatatypes

---
+++ Day_03/Calculator.py
@@ -0,0 +1,16 @@
+#Calculator using only Data types 
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+sum= num1 + num2;
+subs= num1 - num2;
+multi= num1 * num2;
+div = num1 / num2;
+
+print("Addition:",sum)
+print("Substract:",subs)
+print("Multiplication:",multi)
+print("Division:",div)
+
+
+
