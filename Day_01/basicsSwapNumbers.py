93751d0a778f5696c4d31d03cdae383e91cc92fd
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-14 03:43:38 +0000

    swap two numbers

---
+++ Day_01/basicsSwapNumbers.py
@@ -0,0 +1,29 @@
+
+#Write A program to swap two number using three variables 
+
+num1 =809
+num2 =575
+
+print("Before swap=",num1)
+print("Before swap=",num2)
+
+temp=num1
+num1=num2
+num2=temp
+
+print("After swap=" ,num1)
+print("After swap=" ,num2)
+
+
+#Write a program to swap of two number without third variables
+
+num1=700
+num2=500
+
+print("Before Swap=", num1)
+print("Before Swap=", num1)
+
+
+num1, num2=num2,num1
+print("After Swap=",num1)
+print("After Swap=",num2)
