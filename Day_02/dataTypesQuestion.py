d45f4aaf8ec2a979997bc2892ce25bdb5844ec9a
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-17 17:16:13 +0000

    datatypes practice

---
+++ Day_02/dataTypesQuestion.py
@@ -0,0 +1,29 @@
+#Store your name, roll number, and marks in variables. Print their types.
+name = "kumar"
+roll_no = 23
+marks = 98
+
+print("my name is:",type(name))
+print("my roll no is:",type(roll_no))
+print("marks is:",type(marks))
+
+#Write a program to take an integer input from user and print its type
+num1 =int(input("Enter the first number"))
+num2 =int(input("Enter the second number")) 
+
+sum=num1 + num2
+
+print("sum of the number:",sum)
+print("type of the number",type(sum))
+
+
+
+#What is the difference between int, float and complex? Give examples
+num3=300
+num4=7.7
+num5=2+4j
+
+print("number3",type(num3))
+print("number4",type(num4))
+print("number5",type(num5))
+
