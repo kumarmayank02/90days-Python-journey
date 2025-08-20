c2ccecbaca78186d6e273866a03b0af203dfdeb5
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-20 04:17:37 +0000

    lab1

---
+++ PythonLabPractical/Lab1.py
@@ -0,0 +1,11 @@
+#Q.01>Print Hello five times
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+print("Hello")
+
+#Q02.convert fahrenheit to celcius
+fahrenheit=98.6
+celcius=(fahrenheit-32) *5/9
+print("Fahrenheit in to celcius:",celcius)
