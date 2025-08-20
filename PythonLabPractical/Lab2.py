b391fcd2b5b725b2aa00d7abde593dfc059974af
Kumar Mayank <kumarmayank2090@gmail.com>
2025-08-20 04:17:58 +0000

    Lab2

---
+++ PythonLabPractical/Lab2.py
@@ -0,0 +1,19 @@
+# Q03 > WAP to check whether a number is even or not
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("It is even")
+else:
+    print("It is not even")
+
+#Q4>find the largest number from list 
+a=float(input("enter the first number:"))
+b=float(input("enter the second number:"))
+c=float(input("enter the third number:"))
+d=float(input("enter the fourth number:"))
+e=float(input("enter the fifth number:"))
+
+result=[max(a,b,c,d,e)]
+
+print("Final result is :",result)
