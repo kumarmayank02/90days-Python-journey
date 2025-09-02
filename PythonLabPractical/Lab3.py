623751f62eea4554b7b1be47bf879d539e978c5d
Kumar Mayank <kumarmayank2090@gmail.com>
2025-09-02 09:30:32 +0000

    Lab

--- PythonLabPractical/Lab3.py
+++ PythonLabPractical/Lab3.py
@@ -1,29 +1,10 @@
-'''#Write a program to find nth fabonacci number without function 
-
-n = int(input("Enter the value of n: "))
-
-a, b = 0, 1
-
-if n == 0:
-    print(f"The {n}th Fibonacci number is: {a}")
-elif n == 1:
-    print(f"The {n}th Fibonacci number is: {b}")
-else:
- 
-    for i in range(2, n+1):
-        c = a + b
-        a = b
-        b = c
+lst = [1,2,3,4]
+for i in range(0,5):
+    sqr =i**2
+    print(sqr)
     
-    print(f"The {n}th Fibonacci number is: {b}")'''
 
 
-#write a program to sum of nth number
-num = int(input("Enter the number: "))  
-sum = 0 
-for i in range(1, num + 1): 
-    sum += i  
-print("Sum is:", sum)
 
 
 
--- PythonLabPractical/Lab4.py
+++ PythonLabPractical/Lab4.py
@@ -1,12 +1,28 @@
 #Write a program using a  while loop that asks user for a number and print a countdown from that number
 
-num = int(input("Enter a number : "))
+''' num=int(input("Enter the number :")
  
 while num > 0:
     print(num)
     num -= 1
 
-print("Countdown finished!")
-'''
-how we can calculate  
-'''
+print("Countdown finished")'''
+
+#Write a program to count the number of characters in the string and print countdown from the number
+
+
+user_input = input("Enter a string ")
+
+char_count = len(user_input)
+
+print("Number of characters:", char_count)
+
+print("Countdown:")
+for i in range(char_count, 0, -1):
+    print(i)
+
+
+
+
+
+
