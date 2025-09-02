9da55c32ff1de591af66813664d42d6736e0b7d1
Kumar Mayank <kumarmayank2090@gmail.com>
2025-09-02 08:49:07 +0000

    lab4

---
+++ PythonLabPractical/Lab4.py
@@ -0,0 +1,12 @@
+#Write a program using a  while loop that asks user for a number and print a countdown from that number
+
+num = int(input("Enter a number : "))
+ 
+while num > 0:
+    print(num)
+    num -= 1
+
+print("Countdown finished!")
+'''
+how we can calculate  
+'''
