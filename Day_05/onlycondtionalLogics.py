04f94c736c1730739dee410e6f292bb96f929473
Kumar Mayank <kumarmayank2090@gmail.com>
2025-09-15 11:44:24 +0000

    practice

---
+++ Day_05/onlycondtionalLogics.py
@@ -0,0 +1,24 @@
+# Program to check multiples of 5
+'''
+num=int (input("Enter the number:"))
+if num%5==0:
+    print(f"{num} is multiple of number",num)
+else:
+    print(f"{num} is not multiple of number",num)'''
+
+# Program to check leap year
+'''
+year=int (input ("enter the year"))
+if year%4==0:
+    print(f"{year} it is leap year",year)
+else:
+    print(f"{year} it is not leap year",year)'''
+
+#Program to check last digit is even or odd
+
+num=int(input("enter the number"))
+largest=num%10
+if(largest%2==0):
+    print("even")
+else:
+    print("it is not even")
